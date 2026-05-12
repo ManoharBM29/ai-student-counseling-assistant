114029972f27feaebcc2996bfa4375c8f9e8559a
ManoharBM29 <mmanoharabm@gmail.com>
2026-05-12 13:21:45 +0530

    fix api baseURL add/api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { useAuthStore } from '../store/store'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api'
+  // baseURL: 'http://127.0.0.1:8000/api'
+  baseURL: 'https://ai-student-counseling-assistant.onrender.com/api'
 })
 
 api.interceptors.request.use((config) => {
